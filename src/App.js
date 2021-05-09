a326c6e3ca3835436bf9c9f5c98c7c145bc55be8
Manel Akrimi <akrimimanel42@gmail.com>
2021-05-09 13:38:51 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import React ,{Component} from 'react';
+
+class App extends Component {
+  state = {
+    Person: {
+      fullName: "Manel Akrimi",
+      bio: "Hello! Im FullStack JS,based on Sidi Bouzid.I had my Certification from GomyCode.I have found enjoyment and creativity during my period of Learning Developement.",
+      imgSrc: 'Manel.jpg',
+      profession: "WebDeveloper"
+    
+    },
+    show: false,
+    timer: 0,
+    intervall:null
+  };
+  handleShowPerson = () => {
+    this.setState({
+      ...this.state,
+      show: !this.state.show
+    });
+  };
+  componentDidMount(){
+
+    this.setState({
+      intervall : setInterval(() => {
+        this.setState({timer: this.state.timer + 1 }) ;
+
+      }, 1000) ,
+    });
+
+  }
+  componentDidUpdate(){
+    console.log("componentDidUpdate()") ;
+
+  }
+  componentWillUnmount(){
+
+    clearInterval(this.state.timer);
+
+  }
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  <header className="App-header">
+    <img src='profile.svg' className="App-logo" alt="logo" /><br></br>
+    <br/>
+    <h3>Did You want to show profil?</h3>
+</header>
 
-export default App;
+        {this.state.show && (<div className="showprofile">
+  <h1>{this.state.Person.fullName}</h1>
+ 
+  <p>{this.state.Person.bio}</p>
+  <img src={this.state.Person.imgSrc} alt='manel' width={220} height={210}></img> 
+  <h4>{this.state.Person.profession}</h4>
+  <h2>Number of seconds is {this.state.seconds}</h2>
+<h2>{this.state.timer}</h2>
+   </div>
+)}
+
+<button onClick={this.handleShowPerson}>Show</button>
+  </div>
+
+  );
+  }
+  }
+
+  export default App;
